af87a692c532efba7a19a9a18e9ce9a5c87503f8
AliVali7 <161053704+AliVali7@users.noreply.github.com>
2024-07-30 16:48:41 +0400

    Add files via upload

---
+++ PatientDB/PatientDB/PatientDB.DataAccess/Contexts/PatientDBContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatientDB.DataAccess.Configurations;
+using PatientDB.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientDB.DataAccess.Contexts
+{
+	public class PatientDBContext : DbContext
+	{
+        public PatientDBContext(DbContextOptions<PatientDBContext> options) : base(options)
+        {
+            
+        }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfiguration(new UserConfiguration());
+			modelBuilder.ApplyConfiguration(new RoleConfiguration());
+			modelBuilder.ApplyConfiguration(new UserRoleConfiguration());
+			modelBuilder.ApplyConfiguration(new GenderConfiguration());
+			modelBuilder.ApplyConfiguration(new PatientConfiguration());
+		}
+		public DbSet<User> Users { get; set; }
+		public DbSet<Role> Roles { get; set; }
+		public DbSet<UserRole> UserRoles { get; set; }
+		public DbSet<Patient> Patients { get; set; }
+		public DbSet<Gender> Genders { get; set; }
+	}
+}
---
+++ PatientDB/PatientDB/PatientDB.Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientDB.Entities
+{
+	public class BaseEntity
+	{
+        public int Id { get; set; }
+    }
+}
